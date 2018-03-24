f3b966928dcd073ba9f0007e5a297030c73cb12c
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-03-24 12:11:34 +0400

    [ci skip] Perform download url check with HTTP headers from chocolateyInstall.ps1

--- AU/Private/request.ps1
+++ AU/Private/request.ps1
@@ -1,7 +1,8 @@
-function request( [string]$Url, [int]$Timeout ) {
+function request( [string]$Url, [int]$Timeout, $Options ) {
     if ([string]::IsNullOrWhiteSpace($url)) {throw 'The URL is empty'}
     $request = [System.Net.WebRequest]::Create($Url)
     if ($Timeout)  { $request.Timeout = $Timeout*1000 }
+    $Options.Headers | %{$request.Headers.add($_.Name, $_.Value)}
 
     $response = $request.GetResponse()
     $response.Close()
