ac1d467084f269596af40146560912f6331f0e4c
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-07-17 11:41:20 +0300

    [ci skip] Perform download url check with cookies from chocolateyInstall.ps1

--- AU/Private/request.ps1
+++ AU/Private/request.ps1
@@ -3,6 +3,14 @@ function request( [string]$Url, [int]$Timeout, $Options ) {
     $request = [System.Net.WebRequest]::Create($Url)
     if ($Timeout)  { $request.Timeout = $Timeout*1000 }
     if ($Options.Headers) { $Options.Headers.Keys | %{ $request.Headers.add($_, $Options.Headers[$_]) } }
+    if ($Options.CookieStrings) {
+        $cookiejar = New-Object System.Net.CookieContainer
+        $cookiejar.SetCookies($Url, $Options.CookieStrings -join ',')
+        $request.CookieContainer = $cookiejar
+    }
+    if ($Options.CookieContainer) {
+        $request.CookieContainer = $Options.CookieContainer
+    }
 
     $response = $request.GetResponse()
     $response.Close()
