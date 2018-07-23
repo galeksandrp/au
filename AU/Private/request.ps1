9a61561c10cfcccdd80a1bd20919bfc4d673663d
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-07-23 12:53:32 +0300

    Check if Headers is restricted in System.Net.WebRequest
    https://stackoverflow.com/a/4752359/3209640

--- AU/Private/request.ps1
+++ AU/Private/request.ps1
@@ -2,7 +2,16 @@ function request( [string]$Url, [int]$Timeout, $Options ) {
     if ([string]::IsNullOrWhiteSpace($url)) {throw 'The URL is empty'}
     $request = [System.Net.WebRequest]::Create($Url)
     if ($Timeout)  { $request.Timeout = $Timeout*1000 }
-    if ($Options.Headers) { $Options.Headers.Keys | %{ $request.Headers.add($_, $Options.Headers[$_]) } }
+    if ($Options.Headers) { $Options.Headers.Keys | %{
+        # IsRestricted method available only after initializing $request.Headers as WebHeaderCollection
+        $request.Headers.add('X-AU', 'Header')
+        if ($request.Headers.IsRestricted($_)) {
+            $request[$_] = $Options.Headers[$_]
+        } else {
+            $request.Headers.add($_, $Options.Headers[$_])
+        }
+        $request.Headers.remove('X-AU')
+    } }
 
     $response = $request.GetResponse()
     $response.Close()
