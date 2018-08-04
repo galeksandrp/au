d3877a16a008c11c7032d4522e6d972674cbd975
Alexander Georgievskiy <galeksandrp@gmail.com>
2018-08-04 13:00:44 +0300

    [ci skip] Quick and dirty fix of restricted headers passing
    Should be replaced to IsRestricted invokation before passing any header

--- AU/Private/request.ps1
+++ AU/Private/request.ps1
@@ -3,14 +3,11 @@ function request( [string]$Url, [int]$Timeout, $Options ) {
     $request = [System.Net.WebRequest]::Create($Url)
     if ($Timeout)  { $request.Timeout = $Timeout*1000 }
     if ($Options.Headers) { $Options.Headers.Keys | %{
-        # IsRestricted method available only after initializing $request.Headers as WebHeaderCollection
-        $request.Headers.add('X-AU', 'Header')
-        if ($request.Headers.IsRestricted($_)) {
-            $request[$_] = $Options.Headers[$_]
+        if ($_ -eq 'Referer') {
+            $request.Referer = $Options.Headers[$_]
         } else {
             $request.Headers.add($_, $Options.Headers[$_])
         }
-        $request.Headers.remove('X-AU')
     } }
 
     $response = $request.GetResponse()
